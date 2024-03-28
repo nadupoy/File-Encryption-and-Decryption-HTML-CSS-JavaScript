b576d430a497075d8d1571e20696b5c13f173f33
nadupoy <gnsampao@gmail.com>
2024-03-28 12:43:22 +0300

    Encountering errors when trying to use the Crypto JS module.

---
+++ main.js
@@ -0,0 +1,36 @@
+'use strict';
+
+import CryptoJS from './bower_components/crypto-js';
+
+// require.config({
+//     packages: [
+//         {
+//             name: 'crypto-js',
+//             location: 'path-to/bower_components/crypto-js',
+//             main: 'index'
+//         }
+//     ]
+// });
+
+// require(["crypto-js/aes", "crypto-js/sha256"], function (AES, SHA256) {
+//     console.log(SHA256("Message"));
+// });
+
+// const form = document.getElementById('app');
+// const message = document.getElementById('message');
+// const encrypt = document.getElementById('encrypt-button');
+// const decrypt = document.getElementById('decrypt-button');
+// const output = document.getElementById('output');
+
+// app.addEventListener('reset', resetForm);
+// encrypt.addEventListener('click', encryptMessage);
+// decrypt.addEventListener('click', decryptMessage);
+
+// function resetForm() {
+//     output.innerHTML = '';
+// };
+
+// function encryptMessage() {
+//     let messageInput = message.value;
+//     output.innerHTML = messageInput;
+// };
